d362245c8cd3796726ca0135ae830f48d937cf4e
alan-chala <alan.chala000@gmail.com>
2026-04-01 17:32:46 -0500

    refactor(backend): update donation routes

--- backend/src/routes/donationRoutes.js
+++ backend/src/routes/donationRoutes.js
@@ -3,26 +3,19 @@ import {
   createDonation,
   getDonorDonations,
   getAvailableDonations,
+  requestDonation,
   cancelDonation,
-  reserveDonation,
+  completeDonation,
 } from "../controllers/donationController.js";
 import { upload } from "../middleware/uploadMiddleware.js";
 
 const router = express.Router();
 
-// Galería general para beneficiarios
-router.get("/available", getAvailableDonations);
-
-// Historial específico para un donador
-router.get("/donor/:donorId", getDonorDonations);
-
-// Usamos upload.single('imagen') porque el donador solo subirá 1 foto por publicación
 router.post("/", upload.single("imagen"), createDonation);
-
-// Ruta para que el beneficiario reserve (HU-010)
-router.put("/:id/reserve", reserveDonation);
-
-// Historial específico para un donador
-router.delete("/:id/cancel", cancelDonation);
+router.get("/available", getAvailableDonations);
+router.get("/donor/:donorId", getDonorDonations);
+router.put("/request/:id", requestDonation);
+router.put("/cancel/:id", cancelDonation);
+router.put("/complete/:id", completeDonation);
 
 export default router;
