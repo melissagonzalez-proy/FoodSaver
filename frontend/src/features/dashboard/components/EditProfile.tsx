06166b191f9d151c4ca2e45e2958db5c20a22c64
alan-chala <alan.chala000@gmail.com>
2026-04-24 15:10:52 -0500

    fix: delete no used code

--- frontend/src/features/dashboard/components/EditProfile.tsx
+++ frontend/src/features/dashboard/components/EditProfile.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { apiUrl } from "../../../lib/api"; 
 import { CheckCircle, AlertCircle, Save } from "lucide-react";
